08235a2e29369af58ebfd39b9eb7d288f9e0f973
Anuj Rai <anuj.rai2489@gmail.com>
2020-04-27 00:38:00 +0530

    Add files via upload
    project setup

---
+++ PictureGallery/PictureGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PictureGallery
+//
+//  Created by Anuj Rai on 27/04/20.
+//  Copyright © 2020 Anuj Rai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
